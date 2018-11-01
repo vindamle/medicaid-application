9b088c6a00581eb3f4453bdcc2113b667a529833
David Klaver <DKlaver@bcrhc.local>
2018-11-01 14:30:50 -0400

    AJAX for ignore button on activity page

--- static/js/activity.js
+++ static/js/activity.js
@@ -1,39 +1,6 @@
-
-const trackSelectedBtn = document.getElementById('trackSelectedBtn');
-
-// document.addEventListener('click', (e) => {
-// //change row color when clicking/unclicking 'Track' button
-// 	if (e.target.parentNode.classList.contains('trackBtnCell')) {
-// 		const button = e.target;
-// 		if (e.target.textContent == 'Track') {
-// 			e.target.textContent = '\u2713';
-// 			button.className = 'btn-tracked';
-// 		} else {
-// 			e.target.textContent = 'Track';
-// 			button.className = 'btn-track';
-// 		};
-// // 'Ignore' button removes row - demo functionality (doesn't affect database yet)
-// 	} else if (e.target.className == 'btn-ignore') {
-// 		if (confirm("Are you sure you want ignore this alert?")) {
-// 			const row = e.target.parentNode.parentNode;
-// 			const rowParent = row.parentNode;
-// 			rowParent.removeChild(row);
-// 		};
-// // 'Track Selected' button removes all tracked rows - demo functionality (doesn't affect database yet)
-// 	} else if (e.target == trackSelectedBtn) {
-// 		const tracked = document.querySelectorAll('.btn-tracked');
-// 		console.log(tracked);
-// 		for (var i = 0; i < tracked.length; i++) {
-// 			const row = tracked[i].parentNode.parentNode;
-// 			console.log(row);
-// 			row.parentNode.removeChild(row);
-// 		};
-// 	};
-// });
-
-//jQuery AJAX request to track or ignore residents
+// jQuery AJAX request to track or ignore residents and remove resident's row from view upon success
 $('.btn-track').click(function(){
-	const row = this.parentNode.parentNode;
+    const row = this.parentNode.parentNode;
     $.ajax(
     {
         type:"GET",
@@ -48,3 +15,52 @@ $('.btn-track').click(function(){
         }
      })
 });
+
+$('.btn-ignore').click(function(){
+  if (confirm("Are you sure you want ignore this alert?")) {
+    const row = this.parentNode.parentNode;
+    $.ajax(
+      {
+        type:"GET",
+        url: "/application/ajax/track_or_ignore",
+        data:
+          {
+            patient_id: row.id,
+            tracking: false
+          },
+        success: function() 
+          {
+            row.parentNode.removeChild;
+          }
+     });
+   };
+});
+
+// const trackSelectedBtn = document.getElementById('trackSelectedBtn');
+
+// document.addEventListener('click', (e) => {
+// //change row color when clicking/unclicking 'Track' button
+// 	if (e.target.parentNode.classList.contains('trackBtnCell')) {
+// 		const button = e.target;
+// 		if (e.target.textContent == 'Track') {
+// 			e.target.textContent = '\u2713';
+// 			button.className = 'btn-tracked';
+// 		} else {
+// 			e.target.textContent = 'Track';
+// 			button.className = 'btn-track';
+// 		};
+//   } else if (e.target.className == 'btn-ignore') {
+// 		if (confirm("Are you sure you want ignore this alert?")) {
+// 			const row = e.target.parentNode.parentNode;
+// 			$(row).remove();
+// 		} else if (e.target == trackSelectedBtn) {
+// 		const tracked = document.querySelectorAll('.btn-tracked');
+// 		console.log(tracked);
+// 		for (var i = 0; i < tracked.length; i++) {
+// 			const row = tracked[i].parentNode.parentNode;
+// 			console.log(row);
+// 			$(row).remove();
+// 		};
+// 	};
+// };
+// });
