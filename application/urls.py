d30a79d2121975058e7feeb1c1ff92723bf7f3b4
MarcPfeif <MPfeiffer@centershealthcare.org>
2018-11-12 15:19:22 -0500

    New URL

--- application/urls.py
+++ application/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('signup/', views.signup.as_view(), name='signup'),
     path('create/', views.create.as_view(), name = 'create'),
     path('ajax/track_or_ignore/' ,views.update_list , name = "update_list"),
+    path('ajax/file_uploader/', views.file_upload, name = 'file_upload')
 
 ]
