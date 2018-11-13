209cc6c138d3ea146c402a5a2f9e351b78efb722
David Klaver <DKlaver@bcrhc.local>
2018-11-13 10:08:42 -0500

    file uploads as forms

--- static/js/show.js
+++ static/js/show.js
@@ -1,44 +1,36 @@
-// jQuery AJAX request to track or ignore residents, then remove resident's row from view upon success
-
-const editBtn = document.querySelector("#editBtn");
+//file upload button functionality
 const patientId = document.querySelector("#patientId").innerHTML;
-// const uploadFile = (patient_id, file) => {
-// 	const ajaxCall = $.ajax(
-// 	{
-// 		type:"GET",
-// 		url: "/application/ajax/file_uploader",
-// 		data:{
-// 			patient_id: patient_id,
-// 			file: file
-// 		},
-// 		success: function() 
-// 		{
-		
-// 		}
-// 	});
-// 	return(ajaxCall);
-// };
-
-$(editBtn).click(() => {
-	const applicationFile = document.getElementById('medicaidApplicationFile');
-	const file = applicationFile.files[0];
-	// uploadFile(patientId, file);
+const uploadFile = (fileType, file) => {
 	const ajaxCall = $.ajax(
 	{
 		type:"GET",
+		url: "/application/ajax/file_uploader",
 		contentType: false,
 		processData: false,
-		url: "/application/ajax/file_uploader",
 		data:{
 			patient_id: patientId,
+			fileType: fileType,
 			file: file
 		},
-		success: function() 
+		success: function(response) 
 		{
-			alert('file uploaded!');
+			console.log(response);
+			alert(`File uploaded! (Response: ${response})`);
 		}
 	});
 	return(ajaxCall);
+};
+
+$('.uploadBtn').click((e) => {
+	li = e.target.parentNode;
+	const fileType = li.id;
+	const file = li.querySelector('input').files[0];
+	console.log(file);
+	if (file) {
+		uploadFile(fileType, file);
+	} else {
+		alert("Please select a file to upload.")
+	};	
 });
 
 
@@ -56,39 +48,39 @@ $(editBtn).click(() => {
 
 
 // Toggle fields in and out of editable state on show pages
-// const editBtn = document.querySelector("#editBtn");
-// editBtn.addEventListener('click', (e) => {	
-// 	const editableItems = document.querySelectorAll('.editable');
-// 	if (editBtn.textContent == 'Edit') {
-// 			for (var i = 0; i < editableItems.length; i++) {
-// 			const	editableItem = editableItems[i]
-// 			const input = document.createElement('input');
-// 			input.type = 'text';
-// 			input.value = editableItem.textContent;
-// 			input.className = 'editable';
-// 			const parent = editableItem.parentNode;
-// 			parent.insertBefore(input, editableItem);
-// 			parent.removeChild(editableItem);
-// 			parent.style.borderBottom="none";
-// 			const ul = parent.parentNode;
-// 			ul.style.textAlign = 'right';
-// 			editBtn.textContent = 'Save';
-// 			editBtn.classList.toggle('saveBtn');
-// 		};
-// 	} else if (editBtn.textContent == 'Save') {
-// 			for (var i = 0; i < editableItems.length; i++) {
-// 			const editableItem = editableItems[i]
-// 			const span = document.createElement('span');
-// 			span.textContent = editableItem.value;
-// 			span.className = 'editable';
-// 			const parent = editableItem.parentNode;
-// 			parent.insertBefore(span, editableItem);
-// 			parent.removeChild(editableItem);
-// 			parent.style.borderBottom="1px solid #ddd";
-// 			const ul = parent.parentNode;
-// 			ul.style.textAlign = 'left';
-// 			editBtn.textContent = 'Edit';
-// 			editBtn.classList.toggle('saveBtn');
-// 		};
-// 	};
-// });
+const editBtn = document.querySelector("#editBtn");
+editBtn.addEventListener('click', (e) => {	
+	const editableItems = document.querySelectorAll('.editable');
+	if (editBtn.textContent == 'Edit') {
+			for (var i = 0; i < editableItems.length; i++) {
+			const	editableItem = editableItems[i]
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.value = editableItem.textContent;
+			input.className = 'editable';
+			const parent = editableItem.parentNode;
+			parent.insertBefore(input, editableItem);
+			parent.removeChild(editableItem);
+			parent.style.borderBottom="none";
+			const ul = parent.parentNode;
+			ul.style.textAlign = 'right';
+			editBtn.textContent = 'Save';
+			editBtn.classList.toggle('saveBtn');
+		};
+	} else if (editBtn.textContent == 'Save') {
+			for (var i = 0; i < editableItems.length; i++) {
+			const editableItem = editableItems[i]
+			const span = document.createElement('span');
+			span.textContent = editableItem.value;
+			span.className = 'editable';
+			const parent = editableItem.parentNode;
+			parent.insertBefore(span, editableItem);
+			parent.removeChild(editableItem);
+			parent.style.borderBottom="1px solid #ddd";
+			const ul = parent.parentNode;
+			ul.style.textAlign = 'left';
+			editBtn.textContent = 'Edit';
+			editBtn.classList.toggle('saveBtn');
+		};
+	};
+});
