d00e3c1af4601b312246b4337d492fc0945c3dac
MarcPfeif <mpfeiffer@centershealthcare.org>
2019-04-15 10:13:44 -0400

    Updates

--- app/settings.py
+++ app/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'medicaid',
-        # 'USER': 'marc',
-        'USER': 'postgres',
-        # 'PASSWORD': 'Aug.2018',
-        'PASSWORD': 'Oct2018!',
+        'USER': 'marc',
+        # 'USER': 'postgres',
+        'PASSWORD': 'Aug.2018',
+        # 'PASSWORD': 'Oct2018!',
     }
 }
 
@@ -135,5 +135,7 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
